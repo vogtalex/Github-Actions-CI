c9dc63e57ccdbee29d13e3b199fb6b40c6533c80
Alex Vogt <vogtalex@oregonstate.edu>
2021-06-03 17:50:38 -0700

    Re-fixed lint errors

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import example
 
 class TestCase(unittest.TestCase):
 
-	def test_add_1(self):
-		self.assertEqual(example.add(1,2), 3)
+    def test_add_1(self):
+        self.assertEqual(example.add(1, 2), 3)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
